db6579961c5184bf85f856e95c69ef8aeb6ef46b
therealkuma <108306648+therealkuma@users.noreply.github.com>
2023-08-29 10:21:59 -0400

    Update app.py

--- app.py
+++ app.py
@@ -36,7 +36,7 @@ if authentication_status:
                 st.success('Please logout and log back in')
                 
                 #Store user information in Deta
-                user_data = {"key": new_username, "name": new_name, "email":new_email, "password": new_password}
+                user_data = {"key": username, "name": name, "email": email, "password": password}
                 jb.db.put(user_data)
         except Exception as e:
             st.error(e)
