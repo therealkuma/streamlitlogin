d18d9a96f6027f8422455fb0a6aea3f894351ec4
therealkuma <108306648+therealkuma@users.noreply.github.com>
2023-08-29 16:49:55 -0400

    Update app.py

--- app.py
+++ app.py
@@ -36,10 +36,10 @@ if authentication_status:
                 st.success('Please logout and log back in')
                 
                 # access the last username of credentials
-                new_username= list(authenticator.credentials['usernames'].keys())[-1])
-                new_name= list(authenticator.credentials['names'].keys())[-1])
+                new_username= list(authenticator.credentials['usernames'].keys())[-1]
+                new_name= list(authenticator.credentials['names'].keys())[-1]
                 #Store user information in Deta
-                user_data = {"key": username, "name": name}
+                user_data = {"key": new_username, "name": new_name}
                 jb.db.put(user_data)
         except Exception as e:
             st.error(e)
