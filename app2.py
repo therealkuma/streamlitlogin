144859ad9cec87a53374e0de176047b45efb1192
therealkuma <108306648+therealkuma@users.noreply.github.com>
2023-08-29 18:05:06 -0400

    Update app2.py

--- app2.py
+++ app2.py
@@ -12,7 +12,11 @@ import plotly.graph_objects as go
 import database as jb
 import streamlit as st
 import streamlit_authenticator as stauth
+import yaml
+from yaml.loader import SafeLoader
 
+with open('config.yaml') as file:
+    config = yaml.load(file, Loader=SafeLoader)
 
 authenticator = stauth.Authenticate(
     config['credentials'],
