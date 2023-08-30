43e81a0d4249a10ca6ea31ce3efaacaf85c95622
therealkuma <108306648+therealkuma@users.noreply.github.com>
2023-08-30 15:19:33 -0400

    Update bg.py

--- bg.py
+++ bg.py
@@ -16,19 +16,20 @@ img = get_img_as_base64("image.jpg")
 page_bg_img = f"""
 <style>
 [data-testid="stAppViewContainer"] > .main {{
-background-image: url("https://images.unsplash.com/photo-1501426026826-31c667bdf23d");
+background-image:  url("data:image/png;base64,{img}");
+# url("https://images.unsplash.com/photo-1501426026826-31c667bdf23d");
 background-size: 180%;
 background-position: top left;
 background-repeat: no-repeat;
 background-attachment: local;
 }}
 
-[data-testid="stSidebar"] > div:first-child {{
-background-image: url("data:image/png;base64,{img}");
-background-position: center; 
-background-repeat: no-repeat;
-background-attachment: fixed;
-}}
+# [data-testid="stSidebar"] > div:first-child {{
+# background-image: url("data:image/png;base64,{img}");
+# background-position: center; 
+# background-repeat: no-repeat;
+# background-attachment: fixed;
+# }}
 
 [data-testid="stHeader"] {{
 background: rgba(0,0,0,0);
